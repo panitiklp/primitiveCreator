ce978b2800a7a750a1b388a8aad9c3141146318d
lab1 ict <lab1@email.com>
2025-09-27 12:09:31 +0700

    add run function ready to maya

--- primitiveCreatorUi.py
+++ primitiveCreatorUi.py
@@ -1,4 +1,26 @@
-from PySide6 import QtCore, QtGui, QtWidgets
-from shiboken6 import wrapInstance
+try:
+	from PySide6 import QtCore, QtGui, QtWidgets
+	from shiboken6 import wrapInstance
+except:
+	from PySide2 import QtCore, QtGui, QtWidgets
+	from shiboken2 import wrapInstance
+
 import maya.OpenMayaUI as omui
 
+class PrimitiveCreatorDialog(QtWidgets.QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.resize(300,300)
+		self.setWindowTitle('Primitive Creator')
+
+def run():
+	global ui
+
+	try:
+		ui.close()
+	except:
+		pass
+	ptr = wrapInstance(int(omui.MQtUtil.mainWindow()), QtWidgets.QWidget)
+	ui = PrimitiveCreatorDialog(parent=ptr)
+	ui.show()
